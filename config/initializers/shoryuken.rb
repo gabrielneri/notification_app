c8a7a797e446a6e79ce01f366c5e7befedc698ef
Gabriel <gasantosneri@hotmail.com>
2026-02-18 02:37:51 -0300

    chore: update SQS config for IAM role and add environment sample file

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,21 +1,24 @@
 return if Rails.env.test?
 
-Shoryuken.configure_client do |config|
-  config.sqs_client = Aws::SQS::Client.new(
+def sqs_client_options
+  options = {
     region: ENV.fetch('AWS_REGION'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    endpoint: ENV.fetch('AWS_SQS_ENDPOINT'),
     verify_checksums: false
-  )
+  }
+
+  if Rails.env.development?
+    options[:access_key_id] = ENV.fetch('AWS_ACCESS_KEY_ID')
+    options[:secret_access_key] = ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    options[:endpoint] = ENV.fetch('AWS_SQS_ENDPOINT')
+  end
+
+  options
+end
+
+Shoryuken.configure_client do |config|
+  config.sqs_client = Aws::SQS::Client.new(**sqs_client_options)
 end
 
 Shoryuken.configure_server do |config|
-  config.sqs_client = Aws::SQS::Client.new(
-    region: ENV.fetch('AWS_REGION'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    endpoint: ENV.fetch('AWS_SQS_ENDPOINT'),
-    verify_checksums: false
-  )
+  config.sqs_client = Aws::SQS::Client.new(**sqs_client_options)
 end
